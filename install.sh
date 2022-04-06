1b87f07ce30ac8bd03a0689aa07cbb93439ca7a1
R.Grouls <raoul.grouls@businessdecision.nl>
2022-04-06 14:12:49 +0200

    [FIX]

--- install.sh
+++ install.sh
@@ -5,20 +5,18 @@ sudo apt -y update ; sudo apt -y install make build-essential libssl-dev zlib1g-
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+if [ -d ~/.pyenv ]; then
+	echo "Pyenv already installed"
+else
+	echo "Installing pyenv"
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+fi
 
 if [ -d "$ZSH" ]; then
 	echo "zsh already installed"
 else
 	echo "installing zsh..."
 	sudo apt install -y zsh
-fi
-	
-
-if [ -d ~/.pyenv ]; then
-	echo "Pyenv already installed"
-else
-	echo "Installing pyenv"
-	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zprofile
 	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
 	echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
