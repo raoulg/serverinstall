7e12e24024d7a223cd725b35b26b19de8f5ceaf1
R.Grouls <raoul.grouls@businessdecision.nl>
2022-04-19 15:42:40 +0200

    counter

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt -y update ; sudo apt -y install make build-essential libssl-dev zlib1g-
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+
 if [ -d ~/.pyenv ]; then
 	echo "Pyenv already installed"
 else
@@ -68,6 +69,32 @@ else
 	sudo apt install tmux
 fi
 
+COUNTER=0
+if [ -d ~/.pyenv ]; then
+	let COUNTER++
+fi
+if [ -d "$ZSH" ]; then
+	let COUNTER++
+fi
+if [ -d ~/.oh-my-zsh ]; then
+	let COUNTER++
+fi
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+	let COUNTER++
+fi
+if [[ $(pyenv versions) == *"3.9.12"* ]]; then
+	let COUNTER++
+fi
+if [ -f $(which poetry) ]; then 
+	let COUNTER++
+fi
+if [ -f $(which autojump) ]; then
+	let COUNTER++
+fi
+if [[ -f $(which tmux) || "$TERM" == "screen" ]]; then
+	let COUNTER++
+fi
+echo "Installed ${COUNTER}/8 items succesfully"
+
 sudo chsh -s $(which zsh)
-echo "INSTALL FINISHED SUCCESFULLY"
 source ~/.zshrc
