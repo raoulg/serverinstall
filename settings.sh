6d27d2f6ddd8bc6dbe7a0e293fb368d3bb292b40
Raoul Grouls <Raoul.Grouls@han.nl>
2024-02-12 12:03:20 +0100

    pdm completions

--- settings.sh
+++ settings.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir $ZSH_CUSTOM/plugins/pdm
+pdm completion zsh > $ZSH_CUSTOM/plugins/pdm/_pdm
 
 echo "adding plugins"
-omz plugin enable zsh-autosuggestions
+omz plugin enable zsh-autosuggestions pdm
 echo "setting theme"
 omz theme set norm
 omz reload
